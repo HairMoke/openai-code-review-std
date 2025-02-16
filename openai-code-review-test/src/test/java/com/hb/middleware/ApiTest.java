0ccdd00226b9332d472806045366872cfc21b222
shall_zhao <shall_zhao@noreply.gitcode.com>
2025-02-16 15:54:38 +0800

    feat: v1-调整SpiTest代码

--- openai-code-review-test/src/test/java/com/hb/middleware/ApiTest.java
+++ openai-code-review-test/src/test/java/com/hb/middleware/ApiTest.java
@@ -1,10 +1,14 @@
 package com.hb.middleware;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
 @RunWith(SpringRunner.class)
-@SpringBoot
+@SpringBootTest
 public class ApiTest {
 
     @Test
