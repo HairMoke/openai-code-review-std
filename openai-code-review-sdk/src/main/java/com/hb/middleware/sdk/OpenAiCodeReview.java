8fb8eeb5c1b3a406e54f1de095b111ef8ff932e8
shall_zhao <shall_zhao@noreply.gitcode.com>
2025-02-16 17:06:41 +0800

    feat: v1-提交main的代码

--- openai-code-review-sdk/src/main/java/com/hb/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/hb/middleware/sdk/OpenAiCodeReview.java
@@ -1,7 +1,30 @@
 package com.hb.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("测试执行");
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffcode = new StringBuilder();
+        while((line = reader.readLine()) != null) {
+            diffcode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+        System.out.println("评审代码：" + diffcode.toString());
     }
 }
