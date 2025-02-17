9814ed09beaf625ec147dc7255d30483497d0f67
shall_zhao <shall_zhao@noreply.gitcode.com>
2025-02-17 15:07:57 +0800

    feat: 代码评审 v4.1

--- openai-code-review-sdk/src/main/java/com/hb/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/hb/middleware/sdk/OpenAiCodeReview.java
@@ -1,13 +1,16 @@
 package com.hb.middleware.sdk;
 
 import com.alibaba.fastjson2.JSON;
+import com.hb.middleware.sdk.domain.model.ChatCompletionRequest;
 import com.hb.middleware.sdk.domain.model.ChatCompletionSyncResponse;
+import com.hb.middleware.sdk.domain.model.Model;
 import com.hb.middleware.sdk.types.utils.BearerTokenUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) throws Exception{
@@ -49,19 +52,16 @@ public class OpenAiCodeReview {
         connection.setRequestProperty("User-Agent","Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
         connection.setDoOutput(true);
 
-        String jsonInputString = "{"
-                + "\"model\":\"glm-4-flash\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + diffCode + "\""
-                + "    }"
-                + "]"
-                + "}";
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
 
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {{
+            add(new ChatCompletionRequest.Prompt("user","你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: "));
+            add(new ChatCompletionRequest.Prompt("user",diffCode));
+        }});
 
         try(OutputStream os = connection.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
 
